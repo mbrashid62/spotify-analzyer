283e4cb996ea506b4aed60999f613a9990d3fd73
Bill Rashid <bill.rashid12@gmail.com>
2019-03-21 18:15:59 -0500

    correctly handling access token

--- src/configs/spotify-auth.js
+++ src/configs/spotify-auth.js
@@ -8,5 +8,5 @@ export let apiCreds = {
 export let wrapperCreds = {
   clientId: 'b3295b28bbbd4d598f32515c7fdad7bf',
   clientSecret: '564da0f10a104edd9ca4f0aabb479ea0',
-  redirectUri: 'http://localhost:3000/callback'
+  redirectUri: process.env.NODE_ENV === 'development' ? 'http://localhost:3000/callback' : 'https://spotify-playlist-tool.herokuapp.com/callback'
 };
